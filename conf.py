84e37bb8f41206a3c9fa7afb0891f401d4676b67
MStenke <51212245+MStenke@users.noreply.github.com>
2020-03-12 13:35:30 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 #import sphinx_bootstrap_theme
-#import sphinx_fontawesome
+import sphinx_fontawesome
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -29,7 +29,10 @@ author = 'Martin Stenke'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo']
+extensions = ['sphinx.ext.todo',
+    'sphinx.ext.ifconfig',
+    'sphinxcontrib.fulltoc',
+    'sphinx_fontawesome']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
